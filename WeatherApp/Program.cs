e1481b86ccb30d3831791e87bd37ce1d2d3b8840
unknown <ruaaaemaa@gmail.com>
2024-02-24 06:52:03 +0200

    initial commit: OpenWeather API

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Net.Http;
+using WeatherApp;
+
+Console.WriteLine(await WeatherAPI.Instance.GetWeather(27.18f, 31.9f));
---
+++ WeatherApp/WeatherAPI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    // Singleton Design Pattern
+    
+    internal class WeatherAPI
+    {
+        const string _url = "https://api.openweathermap.org/";
+        HttpClient _httpClient;
+
+        public readonly static  WeatherAPI Instance = new WeatherAPI();
+
+
+        WeatherAPI()
+        {
+            _httpClient = new HttpClient() { BaseAddress = new Uri(_url) };
+        }
+        public async Task<string> GetWeather(float latitude, float longitude)
+        {
+
+            using HttpResponseMessage response = await _httpClient.GetAsync($"data/2.5/weather?lat={latitude}&lon={longitude}&appid=ca6fd4fd2af88711bdb089e02c43b8a4");
+            return await response.Content.ReadAsStringAsync();
+
+        }
+    }
+}
