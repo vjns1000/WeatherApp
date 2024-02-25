0c7f0ee1fa432772bb85a45d443430c6e115f59f
EightyVice <8yvice@gmail.com>
2024-02-25 13:15:33 +0200

    Geocoding API

--- WeatherApp/WeatherAPI.cs
+++ WeatherApp/WeatherAPI.cs
@@ -13,18 +13,34 @@ namespace WeatherApp
     internal class WeatherAPI
     {
         const string _url = "https://api.openweathermap.org/";
-        HttpClient _httpClient;
+        const string _gecodoing_url = "https://api.api-ninjas.com/";
+        HttpClient _weatherHTTP;
+        HttpClient _gecodingHTTP;
 
         public readonly static  WeatherAPI Instance = new WeatherAPI();
 
 
         WeatherAPI()
         {
-            _httpClient = new HttpClient() { BaseAddress = new Uri(_url) };
+            _weatherHTTP = new HttpClient() { BaseAddress = new Uri(_url) };
+            _gecodingHTTP = new HttpClient() { BaseAddress = new Uri(_gecodoing_url) };
+
+            // Add an Accept header for "application/json" format
+            _gecodingHTTP.DefaultRequestHeaders.Accept.Clear();
+            _gecodingHTTP.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Add any other headers you need, for example, an API key
+            _gecodingHTTP.DefaultRequestHeaders.Add("X-Api-Key", "E+l98iX52w9UuMuHrxrI7A==Pehd2okKKSNF8zLE");
+        }
+        
+        public async Task<string> GetCoords(string city, string country)
+        {
+            using HttpResponseMessage response = await _gecodingHTTP.GetAsync($"v1/geocoding?city={city}&country={country}");
+            return await response.Content.ReadAsStringAsync();
         }
         private async Task<string> GetWeatherJSON(float latitude, float longitude)
         {
-            using HttpResponseMessage response = await _httpClient.GetAsync($"data/2.5/weather?lat={latitude}&lon={longitude}&appid=ca6fd4fd2af88711bdb089e02c43b8a4");
+            using HttpResponseMessage response = await _weatherHTTP.GetAsync($"data/2.5/weather?lat={latitude}&lon={longitude}&appid=ca6fd4fd2af88711bdb089e02c43b8a4");
             return await response.Content.ReadAsStringAsync();
         }
 
