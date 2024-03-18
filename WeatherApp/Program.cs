27cb6b2be9dc46535721616166cae1567016bac8
Roaa Emad Eldin <ruaaaemaa@gmail.com>
2024-03-18 03:40:22 +0200

    First Release ☺

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,16 +1,65 @@
 ﻿using System.Net.Http;
 using WeatherApp;
 using System.Globalization;
+using System.Diagnostics.Metrics;
+
+// For the default screen. Display Asyut weather
+List<GeocodeInfo> CountryCityList = new List<GeocodeInfo>();
+CountryCityList.Add((await GeocodeAPI.Instance.GetGeographyInfo("Asyut", "Egypt"))[0]);
+DisplayWeather(await WeatherAPI.Instance.GetWeather(CountryCityList[0].latitude, CountryCityList[0].latitude));
+while (true)
+{
+    // For entering new cities
+    Console.WriteLine("Press a key to enter new cities.");
+    Console.ReadKey();
+    Console.Clear();
+
+    Console.Write("Enter City: ");
+    string? city = Console.ReadLine();
+
+    Console.Write("Enter Country: ");
+    string? country = Console.ReadLine();
+
+
+    var geocodeInfo = await GeocodeAPI.Instance.GetGeographyInfo(city, country);
+    await CheckGeocodeEnteries(geocodeInfo);
+
+    Thread.Sleep(3000);
+    Console.Clear();
+
+    foreach ( var cityInfo in CountryCityList)
+    {
+        var weather = WeatherAPI.Instance.GetWeather(cityInfo.latitude, cityInfo.longitude);
+        DisplayWeather(await weather);
+      
+    }
+
+}
+async Task CheckGeocodeEnteries(GeocodeInfo[] geocodeInfos)
+{
+    if (geocodeInfos.Length == 0)
+    {
+        Console.WriteLine($"No such city in that country");
+    }
+    else if(geocodeInfos.Length == 1)
+    {
+        CountryCityList.Add(geocodeInfos[0]);
+    }
+    else
+    {
+        HandleSearchEntries(geocodeInfos);
+    }
+}
 
 void DisplayWeather(WeatherProperties weather)
 {
     Console.ForegroundColor = ConsoleColor.White;
     Console.Write($"{(weather.Name + ", " + weather.Country),-20}");
 
-    Console.ForegroundColor = ConsoleColor.Yellow; 
+    Console.ForegroundColor = ConsoleColor.Yellow;
     Console.Write($" ({weather.Temp:0.#}°C)");
 
-    Console.ForegroundColor = ConsoleColor.Blue; 
+    Console.ForegroundColor = ConsoleColor.Blue;
     Console.Write($" ({weather.TempMin:0.#}°C ");
 
     Console.ForegroundColor = ConsoleColor.Red;
@@ -22,27 +71,23 @@ void DisplayWeather(WeatherProperties weather)
     string CapitalizedDescription = TextInfo.ToTitleCase(Description);
     Console.WriteLine($" {CapitalizedDescription}");
 
-    
+
     Console.ResetColor();
 
 }
-
-
-var geocodeInfos = await GeocodeAPI.Instance.GetGeographyInfo("London", "England");
-
-if (geocodeInfos.Length == 0)
+void HandleSearchEntries(GeocodeInfo[] Entries)
 {
-    Console.WriteLine($"No city X in country Y");
-}
-else if (geocodeInfos.Length > 1)
-{
-    foreach(var city in geocodeInfos)
+    int selectedCityInfo = 0;
+    Console.WriteLine("Click the number of targeted city and country");
+    for (int i = 0; i < Entries.Length; i++)
     {
-        var weather = await WeatherAPI.Instance.GetWeather(city.latitude, city.longitude);
-        DisplayWeather(weather);
-    }
-}
-else
-{
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write(i+1 + ") ");
 
+        Console.ResetColor();
+        Console.WriteLine($"{Entries[i].name}, {Entries[i].country}");
+    }
+
+    selectedCityInfo = int.Parse(Console.ReadLine());
+    CountryCityList.Add(Entries[selectedCityInfo - 1]);
 }
